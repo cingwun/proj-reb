7eed256276d7b2cb35f5c068798bc3e4522181b4
Kettan Wu <kettan_wu@xtv-vwd212.(none)>
2014-09-15 10:02:45 +0800

    trysort

--- app/views/spa_admin/articles/view_list.blade.php
+++ app/views/spa_admin/articles/view_list.blade.php
@@ -18,13 +18,14 @@
 			<a href="{{ URL::route('spa.admin.articles.action')}}/0/0/{{$selectedArticles[0]}}" class="btn btn-success" style="float:right;">新增</a>		
 		</div>
 		<div class="table-responsive">
+			<table class="table table-bordered" id="sortable" data-sortAction="<?php echo URL::route('spaAdmin.service_faq.sort.update', array('type'=>$type))?>" data-deleteAction="<?php echo URL::route('admin.service_faq.delete', array('type'=>$type))?>">
 			<table class="table table-bordered" ng-controller="ranksCtrl">
 				<thead>
 					<tr>
 						<th>標題</th>
 						<th>上架日期</th>
 						<th>狀態</th>
-						<th>建立/修改日期</th>
+						<th>時間</th>
 						<th>語言</th>
 					</tr>
 				</thead>
@@ -34,7 +35,7 @@
 						<td>{{ $articles->title}}</td>
 						<td>{{ $articles->open_at}}</td>
 						<td><?php echo ($articles->status=='1') ?  '<span style="color: #00AA00">顯示</span>' : '隱藏'?></td>
-						<td>{{ $articles->created_at."<br>".$articles->updated_at}}</td>
+						<td>{{ "建立:  ".$articles->created_at."<br>"."修改:  ".$articles->updated_at}}</td>
 						<td>{{($articles->lan=='zh') ? '繁體' : '簡體'}}</td>
 						<td><a type="button" href="{{ URL::route('spa.admin.articles.action')}}/{{$articles->id}}" class="btn btn-sm btn-primary">修改</a>
 							<a type="button" href="{{ URL::route('spa.admin.articles.delete')}}/{{$articles->id}}" class="btn btn-sm btn-danger">刪除</a>
@@ -61,7 +62,13 @@
 
 @section('bottom')
 
-{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'); }}
+	{{ HTML::script(asset('packages/tableDnD/js/jquery.tablednd.0.8.min.js'))}}
+    {{ HTML::script(asset('js/admin/service_faq/js_article_list.js'))}}
+    <script type="text/javascript">
+        var sortTable = _sortTable({el: '#sortable', role: 'article', sortColumn: 6, hasCategory: <?php echo (!empty($category))?'true':'false'?>});
+    </script>
+
+<!--{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'); }}
 {{ HTML::script('packages/angularjs/angular.min.js'); }}
 {{ HTML::script('js/admin/app.js'); }}
 
@@ -100,6 +107,6 @@ $(function() {
     });
     $( "#sortable" ).disableSelection();
 });
-</script>
+</script>-->
 
 @stop
