fd84a3a99489c90a2910c5b38e15072862ef2f01
Kettan Wu <kettan_wu@xtv-vwd212.(none)>
2014-10-02 15:31:10 +0800

    set rebeauty layout

--- app/views/admin/member/view_action.blade.php
+++ app/views/admin/member/view_action.blade.php
@@ -1,76 +1,80 @@
-@extends('admin._layouts.default')
+@extends('spa_admin._layouts.default')
+
+@section('title')
+編輯會員
+@stop
 
 @section('main')
-<h2>編輯會員</h2>
-<form action="<?=URL::route('admin.member.action.post')?>" method="post">
+<form action="<?=URL::route('admin.member.action.post')?>" method="post" class="col-lg-4">
 	<div class="form-group">
-		<label class="col-sm-3 control-label">帳號</label>
-		<div class="col-sm-5">
-			<?=$m->uid?>
+		<label>帳號</label>
+		<div>
+			<? //echo $m->uid; ?>
+			<h4>{{ $m->uid;}}</h4>
 		</div>
 	</div>
 	<!-- account -->
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">來源</label>
-		<div class="col-sm-5">
-			<?=$m->social?>
+		<label>來源</label>
+		<div>
+			<h4>{{ $m->social;}}</h4>
 		</div>
 	</div>
 	<!-- social -->
 
 	<?php if ($m->social=='rebeauty'):?>
 	<div class="form-group">
-		<label class="col-sm-3 control-label">密碼</label>
-		<div class="col-sm-5">
-			<input type="text" name="password" value="" />
+		<label>密碼</label>
+		<div>
+			<input type="text" class="form-control" name="password" value="" />
 		</div>
 	</div>
 	<!-- password -->
 	<?php endif;?>
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">E-Mail</label>
-		<div class="col-sm-5">
-			<input type="text" name="email" value="<?=$m->email?>" />
+		<label>E-Mail</label>
+		<div>
+			<input type="text" class="form-control" name="email" value="<?=$m->email?>" />
 		</div>
 	</div>
 	<!-- email -->
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">姓名</label>
-		<div class="col-sm-5">
-			<input type="text" name="name" value="<?=$m->name?>" />
+		<label>姓名</label>
+		<div>
+			<input type="text" class="form-control" name="name" value="<?=$m->name?>" />
 		</div>
 	</div>
 	<!-- name -->
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">生日</label>
-		<div class="col-sm-5">
-			<input type="text" name="birthday" value="<?=$m->birthday?>" />
+		<label>生日</label>
+		<div>
+			<input type="text" class="form-control" name="birthday" value="<?=$m->birthday?>" />
 		</div>
 	</div>
 	<!-- name -->
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">電話</label>
-		<div class="col-sm-5">
-			<input type="text" name="phone" value="<?=$m->phone?>" />
+		<label>電話</label>
+		<div>
+			<input type="text" class="form-control" name="phone" value="<?=$m->phone?>" />
 		</div>
 	</div>
 	<!-- phone -->
 
 	<div class="form-group">
-		<label class="col-sm-3 control-label">地址</label>
-		<div class="col-sm-5">
-			<input type="text" name="address" value="<?=$m->address?>" />
+		<label>地址</label>
+		<div>
+			<input type="text" class="form-control" name="address" value="<?=$m->address?>" />
 		</div>
 	</div>
 	<!-- address -->
 
 	<div class="form-group">
-		<button class="btn" type="button" onclick="history.back();">取消</button> <button class="btn btn-inverse btn-submit">編輯完成</button>
+		<button class="btn btn-danger" type="button" onclick="history.back();">取消</button> <button class="btn btn-primary btn-submit">編輯完成</button>
 	</div>
 
 	<input type="hidden" name="id" value="<?=$m->id?>" />
