0a83e652b5734098c7d1e28ff072619542a5da70
Kettan Wu <kettan_wu@xtv-vwd212.(none)>
2014-10-15 14:30:10 +0800

    fix html bug

--- app/views/admin/articles/index.blade.php
+++ app/views/admin/articles/index.blade.php
@@ -29,33 +29,35 @@ $cc = $category;
       </select>
     </div>
   <div class="pull-right"><a href="{{ URL::to('admin/articles/create?category='.Input::get('category').'&lang='.Input::get('lang')) }}" class="btn btn-success">新增</a></div>
-</div>
+
 <table class="table table-bordered" ng-controller="articlesCtrl" id="clearTop">
-<thead>
-                <tr>
-                  <th>標題</th>
-                  <th>分類</th>
-                  <th>上架日期</th>
-                  <th>狀態</th>
-                  <th>瀏覽數</th>
-                  <th>語言</th>
-                  <th>功能</th>
-                </tr>
-              </thead>
-<tbody id="sortable">
-@foreach ($articles as $article)
-                <tr id="{{ $article->id }}">
-                  <td>{{ $article->title }}</td>
-                  <td>{{ helper::article_category($article->category) }}</td>
-                  <td>{{ $article->open_at }}</td>
-                  <td>{{ ($article->status=='1')?'<span style="color: #00AA00">顯示</span>':'隱藏' }}</td>
-                  <td>{{ $article->views }}</td>
-                  <td>{{ ($article->lang=='tw') ? '繁體' : '簡體' }}</td>
-                  <td><a href="{{ URL::to('admin/articles/'.$article->id.'/edit?category='.Input::get('category').'&lang='.Input::get('lang')) }}" class="btn btn-primary">修改</a> <a href ng-click="deleteArticle({{ $article->id }})" class="btn btn-danger">刪除</a></td>
-                </tr>
-@endforeach
-</tbody>
+    <thead>
+        <tr>
+          <th>標題</th>
+          <th>分類</th>
+          <th>上架日期</th>
+          <th>狀態</th>
+          <th>瀏覽數</th>
+          <th>語言</th>
+          <th>功能</th>
+        </tr>
+    </thead>
+    <tbody id="sortable">
+    @foreach ($articles as $article)
+        <tr id="{{ $article->id }}">
+            <td>{{ $article->title }}</td>
+            <td>{{ helper::article_category($article->category) }}</td>
+            <td>{{ $article->open_at }}</td>
+            <td>{{ ($article->status=='1')?'<span style="color: #00AA00">顯示</span>':'隱藏' }}</td>
+            <td>{{ $article->views }}</td>
+            <td>{{ ($article->lang=='tw') ? '繁體' : '簡體' }}</td>
+            <td><a href="{{ URL::to('admin/articles/'.$article->id.'/edit?category='.Input::get('category').'&lang='.Input::get('lang')) }}" class="btn btn-primary">修改</a> <a href ng-click="deleteArticle({{ $article->id }})" class="btn btn-danger">刪除</a></td>
+        </tr>
+    @endforeach
+    </tbody>
 </table>
+
+</div>
 <?php
 //Here is the acient method to set pager.
 // if(!Input::get('category') || Input::get('category')==3)
