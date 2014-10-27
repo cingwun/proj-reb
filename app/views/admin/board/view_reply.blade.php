32b19e89a167e08ca0eb9e10b09569a64c1c19e8
Leo Wang <leo_wang@xtv-vwd212.(none)>
2014-10-27 16:31:45 +0800

    fix board

--- app/views/admin/board/view_reply.blade.php
+++ app/views/admin/board/view_reply.blade.php
@@ -1,37 +1,43 @@
 @extends('admin._layouts.default')
 
-@section('main')
+@section('title')
+<i class="glyphicon glyphicon-pencil"></i> 美麗留言 - 回覆
+@stop
 
-<h2>回覆留言</h2>
+@section('main')
+<div class="col-lg-12">
 <form action="<?=URL::route('admin.board.reply.store')?>" method="post">
 	<div class="form-group">
-		<label class="col-sm-3 control-label" for="message"><strong>詢問內容</strong></label>
-		<div class="col-sm-5" style="min-height: 150px; padding: 10px 0px;">
+		<label class="control-label" for="message"><strong>詢問內容</strong></label>
+		<div style="min-height: 150px; padding: 10px 0px;">
 			<?=$board['content'];?>
 		</div>
 	</div>
 	<!-- message -->
 
-	@include('admin._partials.widget_labels', array('label'=>$lblWidgetOptions))
-	<!-- tags fields: labels-->
-
 	<div class="form-group">
-		<label class="col-sm-3 control-label" for="content"><strong>回覆內容</strong></label>
-		<div class="col-sm-5">
+		<label class="control-label" for="content"><strong>回覆內容</strong></label>
+		<div>
 			<textarea class="form-control" id="content" name="content" style="width: 650px; min-height: 150px;"><?=Arr::get($reply, 'content', '')?></textarea>
 		</div>
 	</div>
 	<!-- title -->
 
+	<br/>
+	@include('spa_admin._partials.widget_labels', array('label'=>$lblWidgetOptions))
+	<!-- tags fields: labels-->
+
 	<div class="form-group">
 		<div style="margin-top: 10px;">
-			<button class="btn" type="button" onclick="history.back();">取消</button> <button class="btn btn-inverse btn-submit">回覆</button>
+			<button class="btn btn-danger" type="button" onclick="history.back();">取消</button>
+			<button class="btn btn-primary btn-submit">回覆</button>
 		</div>
 	</div>
 
 	<?=Form::token()?>
 	<input type="hidden" name="board_id" value="<?=$board['id']?>" />
 </form>
+</div>
 @stop
 
 
